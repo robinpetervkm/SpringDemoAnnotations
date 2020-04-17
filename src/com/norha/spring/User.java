34859d7aa65374f25fc5dc86c5dc6cdfd9532382
Robinclear <robin.peter.vkm@gmail.com>
2020-04-17 18:35:11 -0300

    After editing the User class finally we got result

--- src/com/norha/spring/User.java
+++ src/com/norha/spring/User.java
@@ -6,7 +6,7 @@ public class User {
 	public static void main(String[] args) {
 		
 		FileSystemXmlApplicationContext context = new FileSystemXmlApplicationContext("Beans.xml");
-		BikeInsurance status = context.getBean("myInsurance",BikeInsurance.class);
+		BikeInsurance status = context.getBean("bikeInsurance",BikeInsurance.class);
 		System.out.println(status.showStatus());
 		
 		context.close();
