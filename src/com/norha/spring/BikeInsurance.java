1b4848b049c09ded31decce72c50e4c27fca5c8d
Robinclear <robin.peter.vkm@gmail.com>
2020-04-17 20:07:15 -0300

    For Demo the Setter Injuction
    cleaning done
    
    
    START from this point

--- src/com/norha/spring/BikeInsurance.java
+++ src/com/norha/spring/BikeInsurance.java
@@ -1,18 +1,9 @@
 package com.norha.spring;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component("myInsurance")
 public class BikeInsurance implements Insurance {
-	
-	public BikeInsurance() {
-		
-	}
-	@Autowired(required=false)
-	public BikeInsurance(Offers offer) {
-		System.out.println(offer.getOffer());
-	}
 
 	@Override
 	public String showStatus() {
--- src/com/norha/spring/Offers.java
+++ src/com/norha/spring/Offers.java
@@ -1,8 +1,5 @@
 package com.norha.spring;
 
-import org.springframework.stereotype.Component;
-
-@Component
 public class Offers {
 	String getOffer() {
 		return "10% offer for ur next purchase";
