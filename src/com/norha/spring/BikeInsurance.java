0ced1358dcf3bb67910014f5bd5b85a206bf7995
Robinclear <robin.peter.vkm@gmail.com>
2020-04-17 20:12:35 -0300

    Added the Data Member tobe injected and added Getter ans setter method

--- src/com/norha/spring/BikeInsurance.java
+++ src/com/norha/spring/BikeInsurance.java
@@ -4,6 +4,16 @@ import org.springframework.stereotype.Component;
 
 @Component("myInsurance")
 public class BikeInsurance implements Insurance {
+	
+	private Offers currentOffer;
+	
+	public Offers getCurrentOffer() {
+		return currentOffer;
+	}
+
+	public void setCurrentOffer(Offers currentOffer) {
+		this.currentOffer = currentOffer;
+	}
 
 	@Override
 	public String showStatus() {
