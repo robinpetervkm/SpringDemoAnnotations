592191e963c814444f7a2998322b4daaf375f847
Robinclear <robin.peter.vkm@gmail.com>
2020-04-17 17:57:33 -0300

    First Commit

---
+++ src/com/norha/spring/BikeInsurance.java
@@ -0,0 +1,13 @@
+package com.norha.spring;
+
+
+public class BikeInsurance implements Insurance {
+	
+
+	@Override
+	public String showStatus() {
+		return "Your Bike is Insured";
+	}
+
+	
+}
---
+++ src/com/norha/spring/User.java
@@ -0,0 +1,14 @@
+package com.norha.spring;
+
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+public class User {
+	public static void main(String[] args) {
+		
+		FileSystemXmlApplicationContext context = new FileSystemXmlApplicationContext("Beans.xml");
+		BikeInsurance status = context.getBean("myInsurance",BikeInsurance.class);
+		System.out.println(status.showStatus());
+		
+		context.close();
+	}
+}
