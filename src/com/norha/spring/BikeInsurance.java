29535b821aa33cc5b4176e6279c054f8aada569f
Robinclear <robin.peter.vkm@gmail.com>
2020-04-17 19:08:38 -0300

    FOR DEMO the AUTOWIRE XML BEAN WITH ANNOTAION
    we have modified above application
    
    START

--- src/com/norha/spring/BikeInsurance.java
+++ src/com/norha/spring/BikeInsurance.java
@@ -1,14 +1,14 @@
 package com.norha.spring;
 
-import org.springframework.stereotype.Component;
-
-@Component
 public class BikeInsurance implements Insurance {
-	
+
+	public BikeInsurance(Offers offer) {
+		System.out.println(offer.getOffer());
+	}
+
 	@Override
 	public String showStatus() {
 		return "Your Bike is Insured";
 	}
 
-	
 }
