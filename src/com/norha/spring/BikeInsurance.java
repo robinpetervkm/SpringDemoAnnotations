5664f6e98deacae8b10b7bdc8172e8eaf6f964ae
Robinclear <robin.peter.vkm@gmail.com>
2020-04-17 18:20:34 -0300

    @Component
    added
    
    but that alone will not help to fix the issue
    
    Still ---------CRASH

--- src/com/norha/spring/BikeInsurance.java
+++ src/com/norha/spring/BikeInsurance.java
@@ -1,9 +1,10 @@
 package com.norha.spring;
 
+import org.springframework.stereotype.Component;
 
+@Component
 public class BikeInsurance implements Insurance {
 	
-
 	@Override
 	public String showStatus() {
 		return "Your Bike is Insured";
